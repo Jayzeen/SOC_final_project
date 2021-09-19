99d8f9a9396daee4dcf5c2d3096a58aaa107605d
Jayzeen <janithjayasinghe.nipuna@gmail.com>
2021-09-19 23:06:46 +0530

    New Home Page Layout

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,21 +1,114 @@
-/**First Slider */
-$('.slider-one')
-.not(".slick-initialized")
-.slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    prevArrow:".site-slider .slider-btn .prev",
-    nextArrow:".site-slider .slider-btn .next"
-});
+//Main Home Page JS
+jQuery(document).ready(function ($) {
 
-/**Second Slider */
-$('.slider-two')
-.not(".slick-initialized")
-.slick({
-    prevArrow:".site-slider-two .prev",
-    nextArrow:".site-slider-two .next",
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplaySpeed: 3000
+    "user strict";
+
+    //Initialize
+
+    var mainSlider = $('.mainSlider');
+    var menuActive = false;
+    var fsOverlay = $('.fs_menu_overlay');
+
+
+    initFavorite();
+    initIsotopeFiltering();
+    initTimer();
+
+
+
+    //Init timer
+    function initTimer() {
+        if ($('.timer').length) {
+
+            var target_date = new Date("September 16 2021").getTime();
+
+            /* var date = new Date();
+            date.setDate(date.getDate() + 3);
+            var target_date = date.getTime(); */
+
+            //Variables for time units
+            var days, hours, minutes, seconds;
+            var d = $('#day');
+            var h = $('#hour');
+            var m = $('#minute');
+            var s = $('#second');
+
+            setInterval(function () {
+                //Find the amount of seconds between now and target date
+
+                var current_date = new Date().getTime();
+                var seconds_left = (target_date - current_date) / 1000;
+
+                //Time calculations
+                days = parseInt(seconds_left / 86400);
+                seconds_left = seconds_left % 86400;
+
+                hours = parseInt(seconds_left / 3600);
+                seconds_left = seconds_left % 3600;
+
+                minutes = parseInt(seconds_left / 60);
+                seconds = parseInt(seconds_left % 60);
+
+                //Display result
+                d.text(days);
+                h.text(hours);
+                m.text(minutes);
+                s.text(seconds)
+
+
+            }, 1000);
+
+        }
+    }
+
+
+    //Init favorite
+    function initFavorite() {
+        if ($('.favorite').length) {
+            var favs = $('.favorite');
+
+            favs.each(function () {
+                var fav = $(this);
+                var active = false;
+                if (fav.hasClass('active')) {
+                    active = true;
+                }
+                fav.on('click', function () {
+                    if (active) {
+                        fav.removeClass('active');
+                        active = false;
+                    }
+                    else {
+                        fav.addClass('active');
+                        active = true;
+                    }
+                });
+
+            });
+        }
+    }
+
+
+    //Init Isotope filtering
+    function initIsotopeFiltering() {
+        if ($('.grid_sorting_button').length) {
+            $('.grid_sorting_button').click(function () {
+                $('.grid_sorting_button.active').removeClass('active');
+                $(this).addClass('active');
+
+                var selector = $(this).attr('data-filter');
+                $('.product-grid').isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+        }
+    }
+
+
 });
